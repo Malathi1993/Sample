fff6c778beddcca018e7b34a9486f28445bf059e
Malathi <57785954+Malathi1993@users.noreply.github.com>
2019-12-02 14:34:06 +0530

    WebViewEx

---
+++ app/src/main/java/com/example/sample/WebViewActivity.java
@@ -0,0 +1,22 @@
+package com.example.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        WebView mywebview = (WebView) findViewById(R.id.webView);
+        // mywebview.loadUrl("https://www.javatpoint.com/");
+
+        /*String data = "<html><body><h1>Hello, Javatpoint!</h1></body></html>";
+        mywebview.loadData(data, "text/html", "UTF-8"); */
+
+        mywebview.loadUrl("https://developer.android.com");
+    }
+}
