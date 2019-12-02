5bde638b03fc49b50f84e86faf630549fda6cae9
Malathi <57785954+Malathi1993@users.noreply.github.com>
2019-12-02 12:04:17 +0530

    SpinnerEx

---
+++ app/src/main/java/com/example/sample/AutoCopliteTextViewActivity.java
@@ -0,0 +1,26 @@
+package com.example.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class AutoCopliteTextViewActivity extends AppCompatActivity {
+    String[] language ={"C","C++","Java",".NET","iPhone","Android","ASP.NET","PHP"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auto_coplite_text_view);
+        //Creating the instance of ArrayAdapter containing list of language names
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>
+                (this,android.R.layout.select_dialog_item,language);
+        //Getting the instance of AutoCompleteTextView
+        AutoCompleteTextView actv =  (AutoCompleteTextView)findViewById(R.id.autoCompleteTextView);
+        actv.setThreshold(1);//will start working from first character
+        actv.setAdapter(adapter);//setting the adapter data into the AutoCompleteTextView
+        actv.setTextColor(Color.RED);
+    }
+}
